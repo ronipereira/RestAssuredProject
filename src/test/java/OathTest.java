e67e57faae194f90bf8e56c83b9cd36ff3a73b79
Roni Pereira <ronicp@hotmail.com>
2022-05-17 11:32:21 -0300

    feat(): updates till class 66

--- src/test/java/OathTest.java
+++ src/test/java/OathTest.java
@@ -1,7 +1,11 @@
 import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import pojo.GetCourse.Api;
 import pojo.GetCourse.GetCourse;
+import pojo.GetCourse.WebAutomation;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import static io.restassured.RestAssured.*;
 
@@ -10,7 +14,7 @@ public class OathTest {
     public static void main(String[] args) {
 
         //GetCodeUrl = "https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php";
-        String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWgMf1R1piviUF2xl0QxmX9YPUBY_sLtKi09EduBUfdksE_P_ZBb4AF_3M9KTBKZ9A&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent";
+        String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWj2iJKmfe-JgzsBl5tsp4cAN1EmjgwjG-h0fmlRr_fxuQTaB_yqMf38NWxGAzuHAQ&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent";
         String partialCode = url.split("code=")[1];
         String code = partialCode.split("&scope")[0];
 
@@ -36,5 +40,25 @@ public class OathTest {
         System.out.println(getCourse.getCourses().getApi().get(1).getCourseTitle());
 
         List<Api> apiCourses = getCourse.getCourses().getApi();
+
+        for (Api api : apiCourses) {
+            if(api.getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+                System.out.println(api.getPrice());
+        }
+
+        //store courses from webAutomations in one List type variable
+        ArrayList<String> webAutomationCourses = new ArrayList<String>();
+
+        List<WebAutomation> webAutomations = getCourse.getCourses().getWebAutomation();
+        for (WebAutomation webAutomation : webAutomations) {
+            webAutomationCourses.add(webAutomation.getCourseTitle());
+        }
+
+        //Creating a simple array with new courses and then coverting array into ArrayList in order to add to List of courses
+        String[] courseTitles = {"Selenium Webdriver Java", "Cypress", "Protractor"};
+        List<String> expectedList = Arrays.asList(courseTitles);
+
+        Assert.assertTrue(webAutomationCourses.equals(expectedList));
+
     }
 }
